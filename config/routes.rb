ba9bae8ef6e9364c34c6f5889effcc22360308fe
hiiragi17x <hiiragi.en22@gmail.com>
2022-08-06 15:19:57 +0900

    Fix:ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  root 'top#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static_pages#top'
 end
