ff7b65e1f6be7f7ad2771e1751ff13bd5f1061fd
hiiragi17x <hiiragi.en22@gmail.com>
2022-08-10 17:18:57 +0900

    Add:genresテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_104138) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_10_081458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "places", force: :cascade do |t|
     t.string "name", null: false
