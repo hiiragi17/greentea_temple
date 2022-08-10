44a510b0c42e5d7c2d2d51cc6fdb61ac3cda8920
hiiragi17x <hiiragi.en22@gmail.com>
2022-08-10 17:21:34 +0900

    Add:areaテーブルの追加

---
+++ app/models/area.rb
@@ -0,0 +1,3 @@
+class Area < ApplicationRecord
+    validates :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_10_081458) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_10_081914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "areas", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
