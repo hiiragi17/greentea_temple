4e5c26a5226974d36aa2c096b7574592d4083dc6
hiiragi17x <hiiragi.en22@gmail.com>
2022-08-13 17:42:54 +0900

    Fix:Areaモデルの削除

--- app/models/area.rb
+++
@@ -1,6 +0,0 @@
-class Area < ApplicationRecord
-    has_many :places, through: :place_areas
-    has_many :place_areas
-
-    validates :name, presence: true
-end
--- db/migrate/20220810081914_create_areas.rb
+++
@@ -1,9 +0,0 @@
-class CreateAreas < ActiveRecord::Migration[7.0]
-  def change
-    create_table :areas do |t|
-      t.string :name, null: false
-
-      t.timestamps
-    end
-  end
-end
