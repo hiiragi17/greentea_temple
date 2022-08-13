4b0b150e055aa58bf6f6dcaff4af35f77b25b8fd
hiiragi17x <hiiragi.en22@gmail.com>
2022-08-13 19:18:27 +0900

    Fix:model削除のためのテーブル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,58 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_10_083959) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_13_084711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "areas", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "place_areas", force: :cascade do |t|
-    t.bigint "place_id", null: false
-    t.bigint "area_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["area_id"], name: "index_place_areas_on_area_id"
-    t.index ["place_id"], name: "index_place_areas_on_place_id"
-  end
-
-  create_table "place_genres", force: :cascade do |t|
-    t.bigint "place_id", null: false
-    t.bigint "genre_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["genre_id"], name: "index_place_genres_on_genre_id"
-    t.index ["place_id"], name: "index_place_genres_on_place_id"
-  end
-
-  create_table "places", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description", null: false
-    t.string "phone_number"
-    t.string "address", null: false
-    t.string "access", null: false
-    t.datetime "open_time"
-    t.datetime "close_time"
-    t.string "homepage"
-    t.integer "latitude", null: false
-    t.integer "longitude", null: false
-    t.integer "place_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "place_areas", "areas"
-  add_foreign_key "place_areas", "places"
-  add_foreign_key "place_genres", "genres"
-  add_foreign_key "place_genres", "places"
 end
