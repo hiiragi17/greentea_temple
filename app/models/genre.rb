caa711cbcc337c6bc02782a13d745d695f465ed2
hiiragi17x <hiiragi.en22@gmail.com>
2022-08-14 10:31:53 +0900

    Fix:外部キーの修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ApplicationRecord
-    has_many :greenteas, through: :place_genres
+    has_many :greenteas, through: :greentea_genres
     has_many :greentea_genres
 
     validates :name, presence: true
