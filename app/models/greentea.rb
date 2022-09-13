fce66f6d4b54f053c8405b22d5b3e6e5907d7738
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-13 14:21:35 +0900

    add:geokitのロジックを追加 #41

--- app/models/greentea.rb
+++ app/models/greentea.rb
@@ -8,5 +8,11 @@ class Greentea < ApplicationRecord
     validates :name, presence: true
     validates :description, presence: true
 
+    acts_as_mappable :default_units => :kms,
+    :default_formula => :sphere,
+    :distance_field_name => :distance,
+    :lat_column_name => :lat,
+    :lng_column_name => :lng
+
     # enum closed: { open: 0, close: 1 }
 end
--- app/models/temple.rb
+++ app/models/temple.rb
@@ -7,4 +7,10 @@ class Temple < ApplicationRecord
 
     geocoded_by :address
     after_validation :geocode, if: :address_changed?
+
+    acts_as_mappable :default_units => :kms,
+    :default_formula => :sphere,
+    :distance_field_name => :distance,
+    :lat_column_name => :lat,
+    :lng_column_name => :lng
 end
