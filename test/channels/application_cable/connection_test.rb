780c4bedd786abad7d4ea88e1410305355945b01
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-16 15:16:57 +0900

    fix:その他ファイルの修正

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
-  # test "connects with cookies" do
+  # test 'connects with cookies' do
   #   cookies.signed[:user_id] = 42
   #
   #   connect
