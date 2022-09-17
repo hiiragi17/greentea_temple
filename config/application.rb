71fdb2b35ba59e0070df198b409610539226a34b
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-17 14:56:21 +0900

    fix:テストフレームを自動生成するのをやめる #49

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module GreenteaTemple
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.generators do |g|
+      g.test_framework false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
