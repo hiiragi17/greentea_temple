d4edc6dfdb9c82a99f350991d8d27fe661fbce1f
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-18 20:33:12 +0900

    add:factoriesでのデータの作成 #49

--- spec/factories/greenteas.rb
+++ spec/factories/greenteas.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :greentea do
-    
+    sequence(:name) { |n| "抹茶スイーツ#{n}号店" }
+    sequence(:description) { |n| "美味しい抹茶#{n}メニューがある" }
+    phone_number { 075-286-3631 }
+    address { "〒612-0805 京都府京都市伏見区深草藪之内町６８ 伏見稲荷大社啼鳥菴内" }
+    access { "JR奈良線「稲荷駅」から徒歩約5分" }
+    business_hours { "11:00~16:00(L.O.15:30)" }
+    homepage { "https://tsubakido.kyoto/inarisaryo/" }
+    holiday { "水曜日" }
+    sequence(:latitude) { |n| "34.9676#{n}" }
+    sequence(:longitude) { |n| "135.7741#{n}" }
   end
 end
--- spec/factories/temples.rb
+++ spec/factories/temples.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :temple do
-    
+    sequence(:name) { |n| "神社仏閣#{n}号店" }
+    sequence(:description) { |n| "神社仏閣#{n}社がある" }
+    phone_number { 075-371-5181 }
+    address { "〒612-0805 京都府京都市伏見区深草藪之内町６８ 伏見稲荷大社啼鳥菴内" }
+    access { "ＪＲ「京都」駅から徒歩約15分（タクシー5分)" }
+    business_hours { "開門時間5：30～17：00" }
+    homepage { "http://www.hongwanji.kyoto" }
+    holiday { "水曜日" }
+    sequence(:latitude) { |n| "34.9926#{n}" }
+    sequence(:longitude) { |n| "135.7535#{n}" }
   end
 end
