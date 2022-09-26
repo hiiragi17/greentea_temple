23eeac029b53a0cec6ad4ef4a32da8a41ae6c091
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-26 20:50:37 +0900

    fix:Lintチェック #54

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,13 @@
 module ApplicationHelper
-  def page_title(page_title = '', admin = false)
+  def page_title(page_title = '', admin: false)
     base_title = if admin
-                    '抹茶と神社。(管理画面)'
-                else
-                    '抹茶と神社。'
-                end
-  
-    page_title.empty? ? base_title : page_title + ' | ' + base_title
+                   '抹茶と神社。(管理画面)'
+                 else
+                   '抹茶と神社。'
+                 end
+    page_title.empty? ? base_title : "#{base_title}|#{page_title}"
   end
-  
+
   def active_if(path)
     path == controller_path ? 'active' : ''
   end
