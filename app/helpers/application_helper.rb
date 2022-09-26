7343e1d1bc3c31a8f3a5f5e714c37b013717253d
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-26 20:24:55 +0900

    add:動的にタイトルを表示 #54

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def page_title(page_title = '', admin = false)
+    base_title = if admin
+                    '抹茶と神社。(管理画面)'
+                else
+                    '抹茶と神社。'
+                end
+  
+    page_title.empty? ? base_title : page_title + ' | ' + base_title
+  end
+  
+  def active_if(path)
+    path == controller_path ? 'active' : ''
+  end
 end
