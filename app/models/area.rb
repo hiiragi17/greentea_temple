5b5b53b69951e74a329de765fc4569fea4d75f82
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-26 20:14:10 +0900

    fix:管理画面でのエラーを修正 #54

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,6 +1,6 @@
 class Area < ApplicationRecord
-  has_many :temples, through: :temple_areas
   has_many :temple_areas, dependent: :destroy
-
+  has_many :temples, through: :temple_areas
+  
   validates :name, presence: true
 end
--- app/models/greentea.rb
+++ app/models/greentea.rb
@@ -4,8 +4,8 @@ class Greentea < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
-  has_many :genres, through: :greentea_genres
   has_many :greentea_genres, dependent: :destroy
+  has_many :genres, through: :greentea_genres
 
   validates :name, presence: true
   validates :description, presence: true
