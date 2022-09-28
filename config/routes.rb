89af29a21f13f070e356937d578796c8777a3d4d
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-28 15:19:00 +0900

    add:ルーティングの追加 #57

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'static_pages#top'
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
   resources :greenteas, only: %i[index show]
   resources :temples, only: %i[index show] 
+  resources :users
   get 'current_location', to:'current_location#search'
   get 'current_location/result', to:'current_location#result'
 end
