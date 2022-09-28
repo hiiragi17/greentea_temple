7e58875f65d2aa9601c3bbb7ec51c948949bf959
hiiragi17x <hiiragi.en22@gmail.com>
2022-09-28 15:20:49 +0900

    fix:マイグレーションの追加 #57

--- app/controllers/current_location_controller.rb
+++ app/controllers/current_location_controller.rb
@@ -1,4 +1,6 @@
 class CurrentLocationController < ApplicationController
+  skip_before_action :require_login, only: %i[search result]
+
   def search
     gon.greenteas = Greentea.all
     gon.temples = Temple.all
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_081754) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_043522) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_081754) do
     t.string "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "name", default: "ユーザー", null: false
+    t.integer "role", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "greentea_genres", "genres"
