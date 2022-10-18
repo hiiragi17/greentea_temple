f684f6b77cdbb42abaadfb8601459eaf7fd09535
hiiragi17x <hiiragi.en22@gmail.com>
2022-10-18 14:21:45 +0900

    fix:lintチェック #62

--- spec/models/login_module.rb
+++ spec/models/login_module.rb
@@ -4,7 +4,7 @@ module LoginModule
     fill_in 'email', with: user.email
     fill_in 'password', with: 'password'
     within('.login-form') do
-    click_button 'ログインする'
+      click_button 'ログインする'
     end
   end
 end
