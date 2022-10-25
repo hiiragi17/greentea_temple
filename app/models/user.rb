282152de2b56ed3e2112f3061ad4e4c3c594add0
hiiragi17x <hiiragi.en22@gmail.com>
2022-10-25 15:10:58 +0900

    fix:ユーザーの設定#64

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,22 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :authentications, dependent: :destroy
   accepts_nested_attributes_for :authentications
+  has_many :greentea_likes, dependent: :destroy
+  has_many :greenteas, through: :greentea_likes, source: :greentea
 
   validates :name, presence: true
 
   enum role: { general: 0, admin: 1 }
+
+  def greentea_like?(greentea)
+    greenteas.include?(greentea)
+  end
+
+  def greentea_like(greentea)
+   greenteas << greentea
+  end
+  
+  def ungreentea_like(greentea)
+    greenteas.destroy(greentea)
+  end
 end
