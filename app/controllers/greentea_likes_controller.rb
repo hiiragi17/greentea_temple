7efffc6d059ad91915c3dea4195e61f7984fa959
hiiragi17x <hiiragi.en22@gmail.com>
2022-10-31 18:26:33 +0900

    fix:redirect_toに変更 #64

--- app/controllers/greentea_likes_controller.rb
+++ app/controllers/greentea_likes_controller.rb
@@ -2,12 +2,12 @@ class GreenteaLikesController < ApplicationController
   def create
     greentea = Greentea.find(params[:greentea_id])
     current_user.greentea_like(greentea)
-    redirect_back fallback_location: greenteas_path, success: t('.success')
+    redirect_to greenteas_path, success: t('.success')
   end
 
   def destroy
     greentea = current_user.greentea_likes.find(params[:id]).greentea
     current_user.ungreentea_like(greentea)
-    redirect_back fallback_location: greenteas_path, success: t('.success')
+    redirect_to greenteas_path, success: t('.success')
   end
 end
--- app/controllers/temple_likes_controller.rb
+++ app/controllers/temple_likes_controller.rb
@@ -2,12 +2,12 @@ class TempleLikesController < ApplicationController
   def create
     temple = Temple.find(params[:temple_id])
     current_user.temple_like(temple)
-    redirect_back fallback_location: temples_path, success: t('.success')
+    redirect_to temples_path, success: t('.success')
   end
 
   def destroy
     temple = current_user.temple_likes.find(params[:id]).temple
     current_user.untemple_like(temple)
-    redirect_back fallback_location: temples_path, success: t('.success')
+    redirect_to temples_path, success: t('.success')
   end
 end
