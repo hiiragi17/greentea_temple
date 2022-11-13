1b4b170502286d68493ac1513660f775c0cba868
hiiragi17x <hiiragi.en22@gmail.com>
2022-11-13 15:37:44 +0900

    fix:神社詳細ページでお気に入りができないのを修正 #80

--- app/controllers/temple_likes_controller.rb
+++ app/controllers/temple_likes_controller.rb
@@ -2,7 +2,7 @@ class TempleLikesController < ApplicationController
   def create
     temple = Temple.find(params[:temple_id])
     current_user.temple_like(temple)
-    redirect_to temples_path, success: t('.success')
+    redirect_to temple_likes_path, success: t('.success')
   end
 
   def destroy
