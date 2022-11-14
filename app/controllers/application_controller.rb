af099864a16a041320421f99cb23ea0f3f8a588a
hiiragi17x <hiiragi.en22@gmail.com>
2022-11-14 17:29:50 +0900

    fix:lintチェック #80

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :success, :error, :info
 
-  private
+  # private
 
   # def not_authenticated
   #   redirect_to main_app.login_path, info: t('defaults.message.require_login')  # main_appのプレフィックスをつける
