e46177ac374480c808716095f7d32d01487e7634
hiiragi17x <hiiragi.en22@gmail.com>
2022-11-16 18:28:07 +0900

    fix:管理画面のロジック記載 #84

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,12 +6,16 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin
+  before_action :require_login
+  before_action :check_admin 
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      not_authenticated
     end
 
+    def check_admin
+      redirect_to root_path, warning: '権限がありません' unless current_user.admin?
+    end
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
     # def records_per_page
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :success, :error, :info
 
-  # private
+  private
 
-  # def not_authenticated
-  #   redirect_to main_app.login_path, info: t('defaults.message.require_login')  # main_appのプレフィックスをつける
-  # end
+  def not_authenticated
+    redirect_to root_path 
+  end
 end
