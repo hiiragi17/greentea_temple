83c0791f5e5aecb618b60c3732571e44e15901fb
hiiragi17x <hiiragi.en22@gmail.com>
2022-11-17 15:19:17 +0900

    add:エラー用ページを追加 #87

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from Exception, with: :render_500
+
+  def render_404
+    render template: "errors/404", status: :not_found
+  end
+
+  def render_500(exception = nil)
+    render template: "errors/500", status: :internal_server_error
+  end
+
   before_action :require_login
 
   add_flash_types :success, :error, :info
