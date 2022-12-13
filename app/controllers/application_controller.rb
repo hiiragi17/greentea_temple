0dab943e9d2266a41bbf1b27544454a6ea9864fd
hiiragi17x <hiiragi.en22@gmail.com>
2022-12-13 18:22:52 +0900

    fix:ハンドリングを元に戻した　#100

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # rescue_from ActiveRecord::RecordNotFound, with: :render404
-  # rescue_from ActionController::RoutingError, with: :render404
-  # rescue_from Exception, with: :render500
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
+  rescue_from ActionController::RoutingError, with: :render404
+  rescue_from Exception, with: :render500
 
-  # def render404
-  #   render template: 'errors/404', status: :not_found
-  # end
+  def render404
+    render template: 'errors/404', status: :not_found
+  end
 
-  # def render500(_exception = nil)
-  #   render template: 'errors/500', status: :internal_server_error
-  # end
+  def render500(_exception = nil)
+    render template: 'errors/500', status: :internal_server_error
+  end
 
   before_action :require_login
 
