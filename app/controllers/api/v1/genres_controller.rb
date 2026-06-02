fab0b3943f38294bf94a8b9fb4e2c1f4a8b68f8f
hiiragi17 <94298144+hiiragi17@users.noreply.github.com>
2026-06-03 00:09:24 +0900

    Revert "feat: 読み取り系 API エンドポイント実装 (#114)"

--- app/controllers/api/v1/genres_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  module V1
-    class GenresController < BaseController
-      def index
-        render_simple_collection(Genre.order(:id), serializer: GenreSerializer)
-      end
-    end
-  end
-end
--- app/models/temple_area.rb
+++ app/models/temple_area.rb
@@ -1,12 +1,4 @@
 class TempleArea < ApplicationRecord
   belongs_to :temple
   belongs_to :area
-
-  def self.ransackable_attributes(_auth_object = nil)
-    %w[id temple_id area_id]
-  end
-
-  def self.ransackable_associations(_auth_object = nil)
-    %w[temple area]
-  end
 end
